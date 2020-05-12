b2da02fb7e58822552083419cdc45a724c94b87a
SKP <skp.frl@gmail.com>
2020-05-12 21:15:52 +0530

    :sparkles: Add ArticleResolver

--- src/article/article.resolver.ts
+++ src/article/article.resolver.ts
@@ -1,5 +1,34 @@
-import { Resolver } from '@nestjs/graphql';
-import { ArticleModel } from '@/article/article.model';
+import { Resolver, Query, Args, Int, Mutation } from '@nestjs/graphql';
+import { Article } from '@/article/article.model';
+import { ArticleService } from '@/article/article.service';
+import { CreateArticleInput } from '@/article/create-article.input';
+import { EditArticleInput } from '@/article/edit-article.input';
 
-@Resolver(of => ArticleModel)
-export class ArticleResolver {}
+@Resolver(of => Article)
+export class ArticleResolver {
+  constructor(private articleService: ArticleService) {}
+
+  @Query(returns => Article, {name: 'article'})
+  public async getArticle(
+    @Args('id', { type: () => Int }) id: number,
+  ): Promise<Article> {
+    return this.articleService.getArticle(id);
+  }
+
+  @Query(returns => [Article], {name: 'articlesTaggedWith'})
+  public async getArticlesTaggedWith(
+    @Args('tags', { type: () => [String] }) tags: string[],
+  ): Promise<Article[]> {
+    return this.articleService.getArticlesTaggedWith(tags);
+  }
+
+  @Mutation(returns => Article)
+  public async createArticle(createArticleInput: CreateArticleInput): Promise<Article> {
+    return this.articleService.createArticle(createArticleInput);
+  };
+
+  @Mutation(returns => Article)
+  public async editArticle(editArticleInput: EditArticleInput): Promise<Article> {
+    return this.articleService.editArticle(editArticleInput);
+  }
+}
