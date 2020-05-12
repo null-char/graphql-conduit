f43515e937a410f0b0c75d3819dd8611a6912d62
SKP <skp.frl@gmail.com>
2020-05-12 21:13:52 +0530

    :sparkles: Add ArticleEntity

--- src/article/article.entity.ts
+++ src/article/article.entity.ts
@@ -1,4 +1,45 @@
-import { Entity, BaseEntity } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
+import { CommentEntity } from '@/comment/comment.entity';
 
-@Entity()
-export class Article extends BaseEntity {}
+@Entity('article')
+export class ArticleEntity {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  body: string;
+
+  @Column('simple-array')
+  tagList: string[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column({ default: 0 })
+  favoritesCount: number;
+
+  @OneToMany(
+    type => CommentEntity,
+    comment => comment.article,
+    { eager: true },
+  )
+  comments: CommentEntity[];
+
+  // TODO: Add ManyToOne relationship with UserEntity with authorId as FK referencing id from UserEntity
+}
