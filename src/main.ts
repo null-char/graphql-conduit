ef04d2525ed6fb2a9f6c8e5c34df9c97198533af
SKP <skp.frl@gmail.com>
2020-05-15 11:39:07 +0530

    :sparkles: Add cookie-parser middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@/app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api');
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
