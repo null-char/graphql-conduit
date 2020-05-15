c5ce05be3e732a2d5750aaf2e8fdebed4e5bc2cf
SKP <skp.frl@gmail.com>
2020-05-15 11:18:06 +0530

    :recycle: Refactor to use ArticleRepository for database related operations

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,20 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  ForbiddenException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ArticleEntity } from '@/article/article.entity';
-import { Repository } from 'typeorm';
 import { Article } from '@/article/article.model';
 import { CreateArticleInput } from '@/article/create-article.input';
 import { EditArticleInput } from '@/article/edit-article.input';
+import { UserEntity } from '@/user/user.entity';
+import { ArticleRepository } from '@/article/article.repository';
 
 @Injectable()
 export class ArticleService {
   constructor(
-    @InjectRepository(ArticleEntity)
-    private articleRepository: Repository<ArticleEntity>,
+    @InjectRepository(ArticleRepository)
+    private articleRepository: ArticleRepository,
   ) {}
 
   public async getArticle(id: number): Promise<Article> {
-    const article = await this.articleRepository.findOne(id);
+    const article = await this.articleRepository.findArticleById(id);
 
     return {
       ...article,
@@ -31,10 +36,13 @@ export class ArticleService {
 
   public async createArticle(
     createArticleInput: CreateArticleInput,
+    user: UserEntity,
   ): Promise<Article> {
-    const createdArticle = await this.articleRepository.save(
-      createArticleInput,
-    );
+    const article = {
+      ...createArticleInput,
+      authorId: user.id,
+    };
+    const createdArticle = await this.articleRepository.save(article);
 
     return {
       ...createdArticle,
@@ -44,10 +52,13 @@ export class ArticleService {
 
   public async editArticle(
     editArticleInput: EditArticleInput,
+    user: UserEntity,
   ): Promise<Article> {
-    const updatedArticle = await this.articleRepository.findOne(
+    const updatedArticle = await this.articleRepository.findArticleById(
       editArticleInput.id,
     );
+    if (updatedArticle.authorId !== user.id)
+      throw new ForbiddenException('You do not own this resource');
 
     for (const key in editArticleInput) {
       if (editArticleInput[key]) updatedArticle[key] = editArticleInput[key];
