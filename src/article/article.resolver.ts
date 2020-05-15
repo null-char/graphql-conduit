894ab2cec2a06d0705f904b11d1655b317cf774e
SKP <skp.frl@gmail.com>
2020-05-15 11:16:19 +0530

    :sparkles: Add createArticle and editArticle mutations in ArticleResolver

--- src/article/article.resolver.ts
+++ src/article/article.resolver.ts
@@ -3,19 +3,23 @@ import { Article } from '@/article/article.model';
 import { ArticleService } from '@/article/article.service';
 import { CreateArticleInput } from '@/article/create-article.input';
 import { EditArticleInput } from '@/article/edit-article.input';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '@/auth/gql-auth.guard';
+import { GetUser } from '@/shared/get-user.decorator';
+import { UserEntity } from '@/user/user.entity';
 
 @Resolver(of => Article)
 export class ArticleResolver {
   constructor(private articleService: ArticleService) {}
 
-  @Query(returns => Article, {name: 'article'})
+  @Query(returns => Article, { name: 'article' })
   public async getArticle(
     @Args('id', { type: () => Int }) id: number,
   ): Promise<Article> {
     return this.articleService.getArticle(id);
   }
 
-  @Query(returns => [Article], {name: 'articlesTaggedWith'})
+  @Query(returns => [Article], { name: 'articlesTaggedWith' })
   public async getArticlesTaggedWith(
     @Args('tags', { type: () => [String] }) tags: string[],
   ): Promise<Article[]> {
@@ -23,12 +27,22 @@ export class ArticleResolver {
   }
 
   @Mutation(returns => Article)
-  public async createArticle(createArticleInput: CreateArticleInput): Promise<Article> {
-    return this.articleService.createArticle(createArticleInput);
-  };
+  @UseGuards(GqlAuthGuard)
+  public async createArticle(
+    @Args('createArticleInput', { type: () => CreateArticleInput })
+    createArticleInput: CreateArticleInput,
+    @GetUser() user: UserEntity,
+  ): Promise<Article> {
+    return this.articleService.createArticle(createArticleInput, user);
+  }
 
   @Mutation(returns => Article)
-  public async editArticle(editArticleInput: EditArticleInput): Promise<Article> {
-    return this.articleService.editArticle(editArticleInput);
+  @UseGuards(GqlAuthGuard)
+  public async editArticle(
+    @Args('editArticleInput', { type: () => EditArticleInput })
+    editArticleInput: EditArticleInput,
+    @GetUser() user: UserEntity,
+  ): Promise<Article> {
+    return this.articleService.editArticle(editArticleInput, user);
   }
 }
