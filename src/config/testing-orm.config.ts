4ad6dc8079cdfd6835f6c152f4d0a35b048f63ac
SKP <skp.frl@gmail.com>
2020-05-15 11:41:33 +0530

    :wrench: Add a typeorm config for testing (integration tests, e2e, unit testing repositories, etc)

---
+++ src/config/testing-orm.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const TestingOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'graphql_conduit_testing',
+  entities: ['dist/**/*.entity{.ts,.js}', 'src/**/*.entity{.ts,.js}'],
+  synchronize: true,
+  dropSchema: true,
+};
