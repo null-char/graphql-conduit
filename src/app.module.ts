3490366b6f9334ab4253328a557286f2ff927265
SKP <skp.frl@gmail.com>
2020-05-15 11:12:33 +0530

    :sparkles: Add AuthModule, ArticleModule and CommentModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
+import { AuthModule } from '@/auth/auth.module';
+import { ArticleModule } from '@/article/article.module';
+import { CommentModule } from '@/comment/comment.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
+      context: ({ req, res }) => ({ req, res }),
     }),
+    ArticleModule,
+    AuthModule,
+    CommentModule,
   ],
 })
 export class AppModule {}
