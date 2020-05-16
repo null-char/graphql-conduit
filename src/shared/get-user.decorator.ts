c2a21b59a96d97c8ce21d90f4bd32dab82bb6366
SKP <skp.frl@gmail.com>
2020-05-16 13:03:06 +0530

    :recycle: Refactor GetUser decorator to implement functionality in a separate factory function

--- src/shared/get-user.decorator.ts
+++ src/shared/get-user.decorator.ts
@@ -1,12 +1,16 @@
-/* istanbul ignore file */
-
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { UserEntity } from '@/user/user.entity';
 import { GqlExecutionContext } from '@nestjs/graphql';
 
+// exported for testing
+export const getUserFactory = (
+  data: unknown,
+  ctx: ExecutionContext,
+): UserEntity => {
+  const gqlCtx = GqlExecutionContext.create(ctx).getContext();
+  return gqlCtx.req.user;
+};
+
 export const GetUser = createParamDecorator<unknown, any, UserEntity>(
-  (data: unknown, ctx: ExecutionContext): UserEntity => {
-    const gqlCtx = GqlExecutionContext.create(ctx).getContext();
-    return gqlCtx.req.user;
-  },
+  getUserFactory,
 );
