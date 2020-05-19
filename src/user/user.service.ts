11c45b7d8224681e69bbe11b07aa05c8297fde05
SKP <skp.frl@gmail.com>
2020-05-19 12:57:48 +0530

    feat(user): add getProfile method in service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from '@/user/user.entity';
+import { Profile } from '@/user/profile.model';
+import { FollowsEntity } from '@/user/follows.entity';
+import { Repository } from 'typeorm';
 import { UserRepository } from '@/user/user.repository';
 
 @Injectable()
@@ -8,6 +11,8 @@ export class UserService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    @InjectRepository(FollowsEntity)
+    private followsRepository: Repository<FollowsEntity>,
   ) {}
 
   public async createUser(
@@ -16,11 +21,56 @@ export class UserService {
     hashedPassword: string,
     email: string,
   ): Promise<UserEntity> {
-    return this.userRepository.createUser(
+    const userWithSameEmail = await this.userRepository.findOne({
+      where: { email },
+    });
+    if (userWithSameEmail) {
+      throw new ConflictException('User with the same email already exists');
+    }
+
+    const userWithSameUsername = await this.userRepository.findOne({
+      where: { username },
+    });
+    if (userWithSameUsername) {
+      throw new ConflictException('User with same username already exists');
+    }
+
+    const newUser = {
       username,
       salt,
-      hashedPassword,
+      password: hashedPassword,
       email,
+    };
+
+    return this.userRepository.save(newUser);
+  }
+
+  public async getProfile(username: string, user: UserEntity | undefined) {
+    const qb = this.userRepository.createQueryBuilder('u');
+    qb.where('u.username = :username', { username });
+
+    if (user) {
+      qb.addSelect(
+        'u.id IN ' +
+          qb
+            .subQuery()
+            .select('follows.followee')
+            .from(FollowsEntity, 'follows')
+            .where('follows.follower = :uid', { uid: user.id })
+            .getQuery(),
+        'u_following',
+      );
+    }
+
+    return qb.getOne();
+  }
+
+  public async followUser(
+    user: UserEntity,
+    followeeUsername: string,
+  ): Promise<Profile> {
+    const followee = await this.userRepository.findUserByUsername(
+      followeeUsername,
     );
   }
 }
