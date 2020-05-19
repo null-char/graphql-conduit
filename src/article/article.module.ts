543344ab1cd037cf7868bdbc666e95f28308d516
SKP <skp.frl@gmail.com>
2020-05-19 12:26:20 +0530

    feat(article): register FavoritesEntity to TypeOrmModule

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -4,9 +4,15 @@ import { ArticleService } from '@/article/article.service';
 import { AuthModule } from '@/auth/auth.module';
 import { ArticleResolver } from '@/article/article.resolver';
 import { ArticleRepository } from '@/article/article.repository';
+import { FavoritesEntity } from '@/article/favorites.entity';
+import { UserModule } from '@/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ArticleRepository]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([ArticleRepository, FavoritesEntity]),
+    UserModule,
+    AuthModule,
+  ],
   providers: [ArticleService, ArticleResolver],
 })
 export class ArticleModule {}
