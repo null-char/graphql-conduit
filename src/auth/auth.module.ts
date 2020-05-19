da72d8296f1dab472df83f2df90efe83680e5523
SKP <skp.frl@gmail.com>
2020-05-19 12:43:43 +0530

    fix(auth): use forwardRef in imports to resolve circular dependency with UserModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,17 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '@/user/user.module';
 import { AuthService } from '@/auth/auth.service';
 import { JwtStrategy } from '@/auth/jwt.strategy';
 import { SetJwtCookieInterceptor } from '@/auth/set-jwt-cookie.interceptor';
-import { GqlAuthGuard } from '@/auth/gql-auth.guard';
+import { GqlAuthGuard } from '@/auth/guard/gql-auth.guard';
 import { AuthResolver } from '@/auth/auth.resolver';
-import { OptionalAuthGuard } from '@/auth/optional-auth.guard';
+import { OptionalAuthGuard } from '@/auth/guard/optional-auth.guard';
 
 @Module({
   imports: [
-    UserModule,
+    forwardRef(() => UserModule),
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
