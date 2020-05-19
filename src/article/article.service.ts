732ac5913e42926c62af100c5373b221ab1b572c
SKP <skp.frl@gmail.com>
2020-05-19 12:38:46 +0530

    feat(article): add getArticle method in service

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,30 +1,53 @@
 import {
   Injectable,
-  NotFoundException,
   ForbiddenException,
+  NotFoundException,
 } from '@nestjs/common';
+import { Repository, SelectQueryBuilder } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { ArticleEntity } from '@/article/article.entity';
 import { Article } from '@/article/article.model';
-import { CreateArticleInput } from '@/article/create-article.input';
-import { EditArticleInput } from '@/article/edit-article.input';
+import { CreateArticleInput } from '@/article/input/create-article.input';
+import { EditArticleInput } from '@/article/input/edit-article.input';
 import { UserEntity } from '@/user/user.entity';
 import { ArticleRepository } from '@/article/article.repository';
+import { FilterArticlesInput } from '@/article/input/filter-articles.input';
+import { FavoritesEntity } from '@/article/favorites.entity';
+import { ArticleEntity } from '@/article/article.entity';
+import { Profile } from '@/user/profile.model';
+import { UserService } from '@/user/user.service';
+import { UserRepository } from '@/user/user.repository';
 
 @Injectable()
 export class ArticleService {
   constructor(
     @InjectRepository(ArticleRepository)
     private articleRepository: ArticleRepository,
+    @InjectRepository(FavoritesEntity)
+    private favoritesRepository: Repository<FavoritesEntity>,
+    private userService: UserService,
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
   ) {}
 
-  public async getArticle(id: number): Promise<Article> {
-    const article = await this.articleRepository.findArticleById(id);
+  public async getArticle(
+    id: number,
+    user: UserEntity | undefined,
+  ): Promise<Article> {
+    if (!user) {
+      /* 
+        We'll just return favorited field as null for guest users.
+        We expect the client to not allow guest users to favorite articles. Nevertheless, we'll set up a guard for the favoriteArticle mutation.
+      */
+      return this.articleRepository.findArticleById(id);
+    }
 
-    return {
-      ...article,
-      favorited: false,
-    };
+    const qb = this.articleRepository.createQueryBuilder('article');
+    qb.where({ id });
+    this.addFavoritedToArticle(qb, user);
+
+    const article = await qb.getOne();
+    if (!article) throw new NotFoundException('Article not found');
+    return article;
   }
 
   public async getArticlesTaggedWith(tags: string[]): Promise<Article[]> {
