a98c7b5dd3304ed8bb7f1491fc7402dfe434e593
SKP <skp.frl@gmail.com>
2020-05-19 12:44:34 +0530

    fix(user): use forwardRef in imports to resolve circular dependency with AuthModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from '@/user/user.service';
+import { FollowsEntity } from '@/user/follows.entity';
+import { AuthModule } from '@/auth/auth.module';
+import { UserResolver } from '@/user/user.resolver';
 import { UserRepository } from '@/user/user.repository';
 
+/* 
+  Had to resort to forwardRef() here since we need the auth guard in user.resolver.ts
+  Is there a better way of doing this?
+*/
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
-  providers: [UserService],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository, FollowsEntity]),
+    forwardRef(() => AuthModule),
+  ],
+  providers: [UserService, UserResolver],
   exports: [TypeOrmModule, UserService],
 })
 export class UserModule {}
