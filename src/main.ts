ab5bd0b08f828bc3200fc4f1c9853a4b4d3b88a1
SKP <skp.frl@gmail.com>
2020-05-22 21:45:42 +0530

    chore: use express-rate-limit middleware

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,22 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from '@/app.module';
 import cookieParser from 'cookie-parser';
+import rateLimiter from 'express-rate-limit';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('/api');
   app.use(cookieParser());
+  app.use(
+    rateLimiter({
+      windowMs: 10 * 60 * 1000, // 10 mins
+      max: 150,
+    }),
+  );
+  app.set('trust proxy', 1);
   await app.listen(3000);
 }
 bootstrap();
