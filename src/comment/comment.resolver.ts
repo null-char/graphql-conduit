42f4a9caab76035aba60d40a910eb790588d9d49
SKP <skp.frl@gmail.com>
2020-05-22 21:48:12 +0530

    feat(comment): resolve comments queries and create comment mutation

--- src/comment/comment.resolver.ts
+++ src/comment/comment.resolver.ts
@@ -1,5 +1,37 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { Comment } from '@/comment/comment.model';
+import { CommentService } from '@/comment/comment.service';
+import { CreateCommentInput } from '@/comment/input/create-comment.input';
+import { UserEntity } from '@/user/user.entity';
+import { GetUser } from '@/shared/get-user.decorator';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '@/auth/guard/gql-auth.guard';
 
 @Resolver(of => Comment)
-export class CommentResolver {}
+export class CommentResolver {
+  constructor(private commentService: CommentService) {}
+
+  @Query(returns => [Comment], { name: 'comments' })
+  public async getComments(
+    @Args('articleId', { type: () => Int }) articleId: number,
+  ): Promise<Comment[]> {
+    return this.commentService.getComments(articleId);
+  }
+
+  @Query(returns => Comment, { name: 'comment' })
+  public async getComment(
+    @Args('commentId', { type: () => Int }) commentId: number,
+  ): Promise<Comment> {
+    return this.commentService.getComment(commentId);
+  }
+
+  @Mutation(returns => Comment)
+  @UseGuards(GqlAuthGuard)
+  public async createComment(
+    @Args('createCommentInput', { type: () => CreateCommentInput })
+    createCommentInput: CreateCommentInput,
+    @GetUser() user: UserEntity,
+  ): Promise<Comment> {
+    return this.commentService.createComment(createCommentInput, user);
+  }
+}
