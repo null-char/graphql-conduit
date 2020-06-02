58c96748d963e2c34479bca717fdb1dbfcbf2057
SKP <skp.frl@gmail.com>
2020-06-02 21:44:55 +0530

    style: add a synctatic sugar function for returning graphql types

---
+++ src/utils/return-type.ts
@@ -0,0 +1,19 @@
+import { ReturnTypeFuncValue } from '@nestjs/graphql';
+
+/**
+ * A synctatic sugar function which simply takes in a GraphQL type and returns it.
+ * This is useful in the context of GraphQL decorators which takes in an anonymous function for annotating a type.
+ * You can alias this exported function to an appropriate name like: 'of', 'returns', 'type', etc.
+ *
+ * @param t The type to return
+ * @example
+ * // aliased as 'returns'
+ * Query(returns(Int), {name: 'numbers'})
+ * @example
+ * // aliased as 'type'
+ * Field(type(ArticleEntity))
+ */
+export function returnType(t: ReturnTypeFuncValue): () => ReturnTypeFuncValue;
+export function returnType(t: any): any {
+  return t;
+}
